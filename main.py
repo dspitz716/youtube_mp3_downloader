5d9d342dddcd5e59308834b81f1e07028d70598b
Daniel Spitzer <Daniel.Spitzer@bellacanvas.com>
2024-02-13 22:49:51 -0800

    creating web form

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 from utils.youtube_downloader import YoutubeDownloader
+import os
+import tkinter as tk
+from tkinter import filedialog
+
+def main():
+
+    root = tk.Tk()
+    root.withdraw()
+    print("Select location for mp3 download \n>>")
+    dest = filedialog.askdirectory()
+    url = str(input("Enter the URL of the video you want to download: \n>> "))
+    youtube = YoutubeDownloader(url, dest)
+    youtube.download_single()
+
+
+
+if __name__ == '__main__':
+    main()
