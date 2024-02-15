21ea701edd31df01fc9cf7a68a6e39e09648dbd3
Daniel Spitzer <Daniel.Spitzer@bellacanvas.com>
2024-02-14 23:00:22 -0800

    minor changes

---
+++ form.py
@@ -0,0 +1,82 @@
+import tkinter as tk
+from youtube_downloader import YoutubeDownloader
+from tkinter import filedialog
+from tkinter import *
+
+
+class Gui:
+    def __init__(self):
+        self.root = tk.Tk()
+
+    
+
+    def run(self):
+        self.root.geometry("600x400")
+
+        # declaring string variable
+        # for storing name and password
+        url_var = tk.StringVar()
+        play_var = tk.StringVar()
+
+
+        # defining a function that will
+        # get the name and password and
+        # print them on the screen
+        def submit():
+            url = url_var.get()
+            playlist = play_var.get()
+            #password = passw_var.get()
+
+            youtube = YoutubeDownloader(url, filepath, playlist)
+            youtube.download()
+
+            #url_var.set("")
+            #passw_var.set("")
+
+
+        # creating a label for
+        # name using widget Label
+        name_label = tk.Label(self.root, text='Video/Playlist Url', font=('calibre', 10, 'bold'))
+
+        # creating a entry for input
+        # name using widget Entry
+        name_entry = tk.Entry(self.root, textvariable=url_var, font=('calibre', 10, 'normal'))
+
+        # creating a label for password
+        play_label = tk.Label(self.root, text='Download Whole Playlist? y/n (only req. for songs from playlist)', font=('calibre', 10, 'bold'))
+        play_entry = tk.Entry(self.root, textvariable=play_var, font=('calibre', 10, 'normal'))
+
+        # creating a entry for password
+        def browsefunc():
+            global filepath
+            filepath = filedialog.askdirectory()
+            pathlabel.config(text=filepath)
+        
+        browse_label = tk.Label(self.root, text='Download location', font=('calibre', 10, 'bold'))
+
+        browsebutton = Button(self.root, text="Browse", command=browsefunc)
+
+
+        pathlabel = Label(self.root)
+        #passw_var = str(pathlabel)
+
+
+        # creating a button using the widget
+        # Button that will call the submit function
+        sub_btn = tk.Button(self.root, text='Submit', command=submit)
+
+        # placing the label and entry in
+        # the required position using grid
+        # method
+        name_label.grid(row=0, column=0)
+        name_entry.grid(row=0, column=1)
+        play_label.grid(row=1, column=0)
+        play_entry.grid(row=1, column=1)
+        browse_label.grid(row=2, column=0)
+        browsebutton.grid(row=2, column=1)
+        pathlabel.grid(row=2, column=2)
+        sub_btn.grid(row=3, column=1)
+
+        # performing an infinite loop
+        # for the window to display
+        self.root.mainloop()
---
+++ youtube_downloader.py
@@ -0,0 +1,64 @@
+from pytube import YouTube
+from pytube import Playlist
+import os
+
+
+
+class YoutubeDownloader:
+    def __init__(self, url: str, dest: str, playlist: str):
+        self.url = url
+        self.dest = dest
+        self.playlist = playlist
+
+
+    def object_generator(self, url, playlist):
+        url = url
+        if 'com/play' in url:
+            playlist = Playlist(url)
+            pl = []
+            for video in playlist.video_urls:
+                pl.append(video)
+            return playlist
+        
+        elif '&list' in url:
+        
+            yn = str(playlist)
+            if yn.lower() == 'y':
+                playlist = Playlist(url)
+                pl = []
+                for video in playlist.video_urls:
+                    pl.append(video)
+                return playlist
+            elif yn.lower() == 'n':
+                yt = [url]
+                return yt
+            else:
+                print(f'''Please respond y/n''')
+                return None
+                    
+                    
+
+        else:
+            yt = [url]
+            return yt
+
+
+    def download(self):
+        videos = self.object_generator(self.url, self.playlist)
+        try:
+            for video in videos:
+                try:
+                    yt = YouTube(video)
+                    video = yt.streams.filter(only_audio=True).first()
+                    destination = self.dest
+                    out = video.download(output_path=destination)
+                    base, ext = os.path.splitext(out)
+                    new_file = base + '.mp3'
+                    os.rename(out, new_file)
+                    print(yt.title + " has been successfully downloaded.")
+                except KeyError:
+                    print("Unable to fetch video information. Please check the video URL or your network connection.")
+                except FileExistsError:
+                    print("File already exists in location")
+        except TypeError:
+            print("Something went wrong please try again later")
